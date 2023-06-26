23cbb83ecf66a21330aebbcff7d86cde0854aec3
KananPy <kenan.semenderli@gmail.com>
2023-06-26 12:32:50 +0400

    firebase login

---
+++ src/js/firebase/book-library.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD84O_Gy5jdZfcYfergx2kPBu5-APNQ3ug",
+    authDomain: "library-project-ad153.firebaseapp.com",
+    databaseURL: "https://library-project-ad153-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "library-project-ad153",
+    storageBucket: "library-project-ad153.appspot.com",
+    messagingSenderId: "209210870711",
+    appId: "1:209210870711:web:3446eaacf65c7f1e6a7632"
+  };
+  
